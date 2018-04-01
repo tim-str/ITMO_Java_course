de14d8471debf75ee6e2adc5c641594fbf5e1200
tim-str <tim.suyargulov@gmail.com>
2018-04-01 22:09:28 +0300

    The classwork on polymorphism of inheritance - 30/03/18

---
+++ src/assignment2_task7.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class assignment2_task7 {
+
+    public static void main(String[] args) {
+
+        int dim1Array = 5;
+        int dim2Array = 8;
+
+        int[][] rndArray = new int[dim1Array][dim2Array];
+
+        for (int i = 0; i <= dim1Array - 1; i++) {
+            for (int j = 0; j <= dim2Array - 1; j++) {
+                rndArray[i][j] = (int) Math.floor(Math.random() * 199 - 99);
+            }
+
+            System.out.println(Arrays.toString(rndArray[i])); // printing out the array elements generated
+        }
+
+        int max = rndArray[0][0];
+        for (int[] row : rndArray)
+            for (int value : row)
+            {
+                if (value > max) {max = value;}
+            }
+        System.out.println("The maximum value across all the elements of the array is: " + max);
+
+    }
+
+}
